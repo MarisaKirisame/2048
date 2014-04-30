d158bed839ae0715a6233c893051175a0a2b334b
jerrycheng567 <1292652198@qq.com>
2014-04-30 22:33:08 +0800

    add new predicates.

--- player.hpp
+++ player.hpp
@@ -46,74 +46,140 @@ struct player_wrapper : BC
 {
 	player_wrapper( )
 	{
-		this->add_evaluation( []( const core_2048 & ){ return 1; } );
-		for ( size_t i : { 0, 1, 2, 3 } )
+		for ( size_t s : { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 } )
 		{
-			for ( size_t j : { 0, 1, 2, 3 } )
+			this->add_evaluation( [=]( const core_2048 & c ){ return c.empty_square_count( ) == s; } );
+			for ( size_t i : { 0, 1, 2, 3 } )
 			{
-				for ( size_t z : { 0, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072 } )
-				{ this->add_evaluation( [=]( const core_2048 & c ){ return c.data[i][j] == z; } ); }
-				this->add_evaluation( [=]( const core_2048 & c ){ return c.largest_square( ) == c.data[i][j]; } );
-				for ( size_t a : { 0, 1, 2, 3 } )
+				for ( size_t j : { 0, 1, 2, 3 } )
 				{
-					for ( size_t b : { 0, 1, 2, 3 } )
+					for ( size_t z : { 0, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072 } )
+					{ this->add_evaluation( [=]( const core_2048 & c ){ return c.data[i][j] == z && c.empty_square_count( ) == s; } ); }
+					this->add_evaluation( [=]( const core_2048 & c ){ return c.largest_square( ) == c.data[i][j] && c.empty_square_count( ) == s; } );
+					for ( size_t a : { 0, 1, 2, 3 } )
 					{
-						if ( i * 4 + j > a * 4 + b )
+						for ( size_t b : { 0, 1, 2, 3 } )
 						{
-							this->add_evaluation( [=]( const core_2048 & c ){ return c.data[i][j] > c.data[a][b]; } );
-							this->add_evaluation( [=]( const core_2048 & c ){ return ( c.data[i][j] > c.data[a][b] ) && ! c.data[a][b].empty( ); } );
-							this->add_evaluation( [=]( const core_2048 & c ){ return c.data[i][j] == c.data[a][b] ? c.data[i][j] : 0; } );
+							if ( i * 4 + j > a * 4 + b )
+							{
+								for ( size_t z : { 0, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072 } )
+								{
+									this->add_evaluation( [=]( const core_2048 & c )
+									{ return c.data[i][j] > c.data[a][b] && c.data[a][b] == z && c.empty_square_count( ) == s; } );
+									this->add_evaluation( [=]( const core_2048 & c )
+									{ return c.data[i][j] == c.data[a][b] && c.data[a][b] == z && c.empty_square_count( ) == s; } );
+									this->add_evaluation( [=]( const core_2048 & c )
+									{ return c.data[i][j] > c.data[a][b] && c.data[i][j] == z && c.empty_square_count( ) == s; } );
+								}
+							}
 						}
 					}
 				}
 			}
-		}
-		for ( core_2048::direction dir : { core_2048::up, core_2048::down, core_2048::left, core_2048::right } )
-		{ this->add_evaluation( [=]( const core_2048 & c ){ return c.can_move( dir ); } ); }
-		for ( size_t z : { 0, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072 } )
-		{ this->add_evaluation( [=]( const core_2048 & c ){ return c.count( z ); } ); }
-		for ( size_t z : { 0, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072 } )
-		{
-			this->add_evaluation( [=]( const core_2048 & c )
-			{ return ( c.data[0][0] < c.data[0][1] ) && ( c.data[0][0] < c.data[1][0] ) && c.data[0][0] == z; } );
-			this->add_evaluation( [=]( const core_2048 & c )
-			{ return ( c.data[0][3] < c.data[0][2] ) && ( c.data[0][3] < c.data[1][3] ) && c.data[0][3] == z; } );
-			this->add_evaluation( [=]( const core_2048 & c )
-			{ return ( c.data[3][0] < c.data[3][1] ) && ( c.data[3][0] < c.data[2][0] ) && c.data[3][0] == z; } );
-			this->add_evaluation( [=]( const core_2048 & c )
-			{ return ( c.data[3][3] < c.data[3][2] ) && ( c.data[3][3] < c.data[2][3] ) && c.data[3][3] == z; } );
-			this->add_evaluation( [=]( const core_2048 & c )
-			{ return ( c.data[0][1] < c.data[0][0] ) && ( c.data[0][1] < c.data[0][2] ) && c.data[0][1] == z; } );
-			this->add_evaluation( [=]( const core_2048 & c )
-			{ return ( c.data[0][2] < c.data[0][1] ) && ( c.data[0][2] < c.data[0][3] ) && c.data[0][2] == z; } );
-			this->add_evaluation( [=]( const core_2048 & c )
-			{ return ( c.data[1][0] < c.data[0][0] ) && ( c.data[1][0] < c.data[2][0] ) && c.data[1][0] == z; } );
-			this->add_evaluation( [=]( const core_2048 & c )
-			{ return ( c.data[2][0] < c.data[1][0] ) && ( c.data[2][0] < c.data[3][0] ) && c.data[2][0] == z; } );
-			this->add_evaluation( [=]( const core_2048 & c )
-			{ return ( c.data[1][3] < c.data[0][3] ) && ( c.data[1][3] < c.data[2][3] ) && c.data[1][3] == z; } );
-			this->add_evaluation( [=]( const core_2048 & c )
-			{ return ( c.data[2][3] < c.data[1][3] ) && ( c.data[2][3] < c.data[3][3] ) && c.data[2][3] == z; } );
-			this->add_evaluation( [=]( const core_2048 & c )
-			{ return ( c.data[3][1] < c.data[3][0] ) && ( c.data[3][1] < c.data[3][2] ) && c.data[3][1] == z; } );
-			this->add_evaluation( [=]( const core_2048 & c )
-			{ return ( c.data[3][2] < c.data[3][1] ) && ( c.data[3][2] < c.data[3][3] ) && c.data[3][2] == z; } );
-			this->add_evaluation( [=]( const core_2048 & c )
-			{ return ( c.data[0][1] < c.data[0][0] ) && ( c.data[0][1] < c.data[0][2] ) && ( c.data[0][1] < c.data[1][1] ) && c.data[0][1] == z; } );
-			this->add_evaluation( [=]( const core_2048 & c )
-			{ return ( c.data[0][2] < c.data[0][1] ) && ( c.data[0][2] < c.data[0][3] ) && ( c.data[0][2] < c.data[1][2] ) && c.data[0][2] == z; } );
-			this->add_evaluation( [=]( const core_2048 & c )
-			{ return ( c.data[1][0] < c.data[0][0] ) && ( c.data[1][0] < c.data[2][0] ) && ( c.data[1][0] < c.data[1][1] ) && c.data[1][0] == z; } );
-			this->add_evaluation( [=]( const core_2048 & c )
-			{ return ( c.data[2][0] < c.data[1][0] ) && ( c.data[2][0] < c.data[3][0] ) && ( c.data[2][0] < c.data[2][1] ) && c.data[2][0] == z; } );
-			this->add_evaluation( [=]( const core_2048 & c )
-			{ return ( c.data[1][3] < c.data[0][3] ) && ( c.data[1][3] < c.data[2][3] ) && ( c.data[1][3] < c.data[1][2] ) && c.data[1][3] == z; } );
-			this->add_evaluation( [=]( const core_2048 & c )
-			{ return ( c.data[2][3] < c.data[1][3] ) && ( c.data[2][3] < c.data[3][3] ) && ( c.data[2][3] < c.data[2][2] ) && c.data[2][3] == z; } );
-			this->add_evaluation( [=]( const core_2048 & c )
-			{ return ( c.data[3][1] < c.data[3][0] ) && ( c.data[3][1] < c.data[3][2] ) && ( c.data[3][1] < c.data[2][1] ) && c.data[3][1] == z; } );
-			this->add_evaluation( [=]( const core_2048 & c )
-			{ return ( c.data[3][2] < c.data[3][1] ) && ( c.data[3][2] < c.data[3][3] ) && ( c.data[3][2] < c.data[2][2] ) && c.data[3][2] == z; } );
+			for ( core_2048::direction dir : { core_2048::up, core_2048::down, core_2048::left, core_2048::right } )
+			{ this->add_evaluation( [=]( const core_2048 & c ){ return c.can_move( dir ) && c.empty_square_count( ) == s; } ); }
+			for ( size_t ii = 0; ii < 16; ++ii )
+			for ( size_t z : { 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072 } )
+			{ this->add_evaluation( [=]( const core_2048 & c ){ return c.count( z ) == ii && c.empty_square_count( ) == s; } ); }
+			for ( size_t z : { 0, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072 } )
+			{
+				this->add_evaluation( [=]( const core_2048 & c )
+				{ return c.data[0][0] < c.data[0][1] && c.data[0][0] < c.data[1][0] && c.data[0][0] == z && c.empty_square_count( ) == s; } );
+				this->add_evaluation( [=]( const core_2048 & c )
+				{ return c.data[0][3] < c.data[0][2] && c.data[0][3] < c.data[1][3] && c.data[0][3] == z && c.empty_square_count( ) == s; } );
+				this->add_evaluation( [=]( const core_2048 & c )
+				{ return c.data[3][0] < c.data[3][1] && c.data[3][0] < c.data[2][0] && c.data[3][0] == z && c.empty_square_count( ) == s; } );
+				this->add_evaluation( [=]( const core_2048 & c )
+				{ return c.data[3][3] < c.data[3][2] && c.data[3][3] < c.data[2][3] && c.data[3][3] == z && c.empty_square_count( ) == s; } );
+				this->add_evaluation( [=]( const core_2048 & c )
+				{ return c.data[0][1] < c.data[0][0] && c.data[0][1] < c.data[0][2] && c.data[0][1] == z && c.empty_square_count( ) == s; } );
+				this->add_evaluation( [=]( const core_2048 & c )
+				{ return c.data[0][2] < c.data[0][1] && c.data[0][2] < c.data[0][3] && c.data[0][2] == z && c.empty_square_count( ) == s; } );
+				this->add_evaluation( [=]( const core_2048 & c )
+				{ return c.data[1][0] < c.data[0][0] && c.data[1][0] < c.data[2][0] && c.data[1][0] == z && c.empty_square_count( ) == s; } );
+				this->add_evaluation( [=]( const core_2048 & c )
+				{ return c.data[2][0] < c.data[1][0] && c.data[2][0] < c.data[3][0] && c.data[2][0] == z && c.empty_square_count( ) == s; } );
+				this->add_evaluation( [=]( const core_2048 & c )
+				{ return c.data[1][3] < c.data[0][3] && c.data[1][3] < c.data[2][3] && c.data[1][3] == z && c.empty_square_count( ) == s; } );
+				this->add_evaluation( [=]( const core_2048 & c )
+				{ return c.data[2][3] < c.data[1][3] && c.data[2][3] < c.data[3][3] && c.data[2][3] == z && c.empty_square_count( ) == s; } );
+				this->add_evaluation( [=]( const core_2048 & c )
+				{ return c.data[3][1] < c.data[3][0] && c.data[3][1] < c.data[3][2] && c.data[3][1] == z && c.empty_square_count( ) == s; } );
+				this->add_evaluation( [=]( const core_2048 & c )
+				{ return c.data[3][2] < c.data[3][1] && c.data[3][2] < c.data[3][3] && c.data[3][2] == z && c.empty_square_count( ) == s; } );
+				this->add_evaluation( [=]( const core_2048 & c )
+				{
+					return
+							c.data[0][1] < c.data[0][0] &&
+							c.data[0][1] < c.data[0][2] &&
+							c.data[0][1] < c.data[1][1] &&
+							c.data[0][1] == z &&
+							c.empty_square_count( ) == s;
+				} );
+				this->add_evaluation( [=]( const core_2048 & c )
+				{
+					return
+							c.data[0][2] < c.data[0][1] &&
+							c.data[0][2] < c.data[0][3] &&
+							c.data[0][2] < c.data[1][2] &&
+							c.data[0][2] == z &&
+							c.empty_square_count( ) == s;
+				} );
+				this->add_evaluation( [=]( const core_2048 & c )
+				{
+					return
+							c.data[1][0] < c.data[0][0] &&
+							c.data[1][0] < c.data[2][0] &&
+							c.data[1][0] < c.data[1][1] &&
+							c.data[1][0] == z &&
+							c.empty_square_count( ) == s;
+				} );
+				this->add_evaluation( [=]( const core_2048 & c )
+				{
+					return
+							c.data[2][0] < c.data[1][0] &&
+							c.data[2][0] < c.data[3][0] &&
+							c.data[2][0] < c.data[2][1] &&
+							c.data[2][0] == z &&
+							c.empty_square_count( ) == s;
+				} );
+				this->add_evaluation( [=]( const core_2048 & c )
+				{
+					return
+							c.data[1][3] < c.data[0][3] &&
+							c.data[1][3] < c.data[2][3] &&
+							c.data[1][3] < c.data[1][2] &&
+							c.data[1][3] == z &&
+							c.empty_square_count( ) == s;
+				} );
+				this->add_evaluation( [=]( const core_2048 & c )
+				{
+					return
+							c.data[2][3] < c.data[1][3] &&
+							c.data[2][3] < c.data[3][3] &&
+							c.data[2][3] < c.data[2][2] &&
+							c.data[2][3] == z &&
+							c.empty_square_count( ) == s;
+				} );
+				this->add_evaluation( [=]( const core_2048 & c )
+				{
+					return
+							c.data[3][1] < c.data[3][0] &&
+							c.data[3][1] < c.data[3][2] &&
+							c.data[3][1] < c.data[2][1] &&
+							c.data[3][1] == z &&
+							c.empty_square_count( ) == s;
+				} );
+				this->add_evaluation( [=]( const core_2048 & c )
+				{
+					return
+							c.data[3][2] < c.data[3][1] &&
+							c.data[3][2] < c.data[3][3] &&
+							c.data[3][2] < c.data[2][2] &&
+							c.data[3][2] == z &&
+							c.empty_square_count( ) == s;
+				} );
+			}
 		}
 	}
 };
